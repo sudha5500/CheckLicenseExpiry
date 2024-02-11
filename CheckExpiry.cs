00d5355486ec39404b3fd01ccff1a261f755aa50
sudha5500 <33385957+sudha5500@users.noreply.github.com>
2024-02-11 12:07:14 -0500

    Add files via upload

---
+++ CheckExpiry.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CalculateExpiry
+{
+    public class CheckExpiry
+    {
+        public DateTime CalculateExpiryDate(DateTime boughtDate)
+        {
+            DateTime expiryDate = new DateTime();
+            if (boughtDate.Day <= 10)
+            {
+                // end of the month 
+                var startOfMonth = new DateTime(boughtDate.Year, boughtDate.Month, 1);
+                var DaysInMonth = DateTime.DaysInMonth(boughtDate.Year, boughtDate.Month);
+                expiryDate = new DateTime(boughtDate.Year, boughtDate.Month, DaysInMonth);
+            }
+            else if (boughtDate.Day > 10 && boughtDate.Day <= 20)
+            {
+                // end of the month + 10 days
+                var startOfMonth = new DateTime(boughtDate.Year, boughtDate.Month, 1);
+                //var DaysInMonth = DateTime.DaysInMonth(boughtDate.Year, boughtDate.Month);
+                expiryDate = new DateTime(boughtDate.Year, boughtDate.Month+1, 0 + 10);
+            }
+            else if (boughtDate.Day >= 21)
+            {
+                // last day of the next month
+                var startOfMonth = new DateTime(boughtDate.Year, boughtDate.Month, 1);
+                var DaysInMonth = DateTime.DaysInMonth(boughtDate.Year, boughtDate.Month+1);
+                expiryDate = new DateTime(boughtDate.Year, boughtDate.Month + 1, DaysInMonth);
+
+            }
+            return expiryDate.Date;
+
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using CalculateExpiry;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Check your software expiry Date here.");
+        Console.WriteLine("Please enter the software purchase date (in MM-dd-yyyy):");
+        CheckExpiry checkExpiry = new CheckExpiry();
+        // Read input for purchase date
+        if (DateTime.TryParse(Console.ReadLine(), out DateTime purchaseDate))
+        {
+            DateTime expiryDate = checkExpiry.CalculateExpiryDate(purchaseDate);
+            // Display license expiry date
+            Console.WriteLine($"The license will expire on: {expiryDate.ToString("MM-dd-yyyy")}");
+        }
+        else
+        {
+            Console.WriteLine("Invalid Purchase Date. Please enter a valid date");
+        }
+
+        Console.WriteLine("Press any key to exit...");
+        Console.ReadKey();
+    }
+}
