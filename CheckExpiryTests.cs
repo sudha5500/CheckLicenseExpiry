cfe157a975f84c4633cd2a21238342bed3e43a61
sudha5500 <33385957+sudha5500@users.noreply.github.com>
2024-02-11 14:51:24 -0500

    Added Unit Testing project

---
+++ CheckExpiryTests.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculateExpiry;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculateExpiry.Tests
+{
+    [TestClass()]
+    public class CheckExpiryTests
+    {
+        [TestMethod()]
+        public void CalculateExpiryDateTest()
+        {
+            // this is for just one test case with current date as input
+            CheckExpiry checkExpiry = new CheckExpiry();
+            DateTime mockDate = DateTime.Now;
+            DateTime licenseExpDate = checkExpiry.CalculateExpiryDate(mockDate);
+            Assert.AreEqual(licenseExpDate.ToShortDateString(),"3/10/2024");
+        }
+    }
+}
